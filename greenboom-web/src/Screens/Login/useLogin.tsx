62fdd530750e79e89032362a21e261dd1b503f5c
Umair Mehmood <umairkmehmood789@gmail.com>
2024-10-14 18:07:59 +0500

    certain changes

--- greenboom-web/src/Screens/Login/useLogin.tsx
+++ greenboom-web/src/Screens/Login/useLogin.tsx
@@ -2,21 +2,33 @@ import apiService from "../../network/apiServices";
 import { useState } from "react";
 import routes from "../../network/routes";
 import { useNavigate } from "react-router-dom";
-import {showSuccessToast } from "../../common/toast";
+import {showErrorToast, showSuccessToast } from "../../common/toast";
 
 
 function useLogin(){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
 
     
     const navigate = useNavigate();
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [error,setError] = useState('');
 
 
   const handleSubmit = async () => {
    
     console.log("I am getting clicked");
-    loginUserFun(email, password);
+    if(emailRegex.test(email) && passwordRegex.test(password)){
+      loginUserFun(email, password);
+      setError('');
+
+    }
+    else{
+      setError("Invalid Email or Password");
+    }
+    
 
     
   };
@@ -28,20 +40,36 @@ function useLogin(){
           password,
         },
         OnSuccess: res => {
+          if(res.status === 200){
             console.log("user logged in successfully" + res.message);
             showSuccessToast("logged in sucesfully");
-            
             navigate('/');
+          }
+          else{
+            showErrorToast("Incorrect Credentials");
+
+          }
+            
         },
+        //error in api for time being we have added conditions.
         OnError: error => {
-          console.log(error);
+          console.log("error",error);
+          if(error.status === 200){
+            console.log("user logged in successfully" + error.message);
+            showSuccessToast("logged in sucesfully");
+            navigate('/');
+          }else{
+            showErrorToast("Incorrect Credentials");
+
+          }
+          
          
         },
       });
   };
 
 
-  return {email,setEmail,password,setPassword,handleSubmit};
+  return {email,setEmail,password,setPassword,handleSubmit,error,setError};
  
 }
 
