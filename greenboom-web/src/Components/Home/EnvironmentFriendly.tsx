19d3de04ca20fec207aae79b1df0943f7346d426
huzaifa1230 <huzaifairshad173@gmail.com>
2024-10-23 14:06:39 +0500

    feat: home page -> environtmentFriendly section update (new content added)

--- greenboom-web/src/Components/Home/EnvironmentFriendly.tsx
+++ greenboom-web/src/Components/Home/EnvironmentFriendly.tsx
@@ -2,7 +2,14 @@ import Title from "../Title/Title";
 import environment1 from "../../assets/Home/environment1.svg";
 import environment2 from "../../assets/Home/environment2.svg";
 import environment3 from "../../assets/Home/environment.svg";
+import cloud from "../../assets/Home/cloud.svg";
+import hero from "../../assets/Home/hero.png";
+import { useNavigate } from "react-router-dom";
 function EnvironmentFriendly() {
+  const navigate = useNavigate();
+  const handleButton = () => {
+    navigate("/products");
+  };
   return (
     <div className="mt-40">
       <div className="ml-60 mr-60 ">
@@ -35,7 +42,7 @@ function EnvironmentFriendly() {
           >
             MARINE
           </Title>
-          <Title className="text-center " weight="light" fontSize="lg">
+          <Title className="text-center" weight="light" fontSize="lg">
             Green Boom products are lightweight and float on top of the water to
             absorb oil sheens without absorbing a drop of water.
           </Title>
@@ -73,6 +80,36 @@ function EnvironmentFriendly() {
             Green Boom’s tear-resistant moldable socks help control everyday
             leaks with our most shape-hugging sock.
           </Title>
+        </div>
+      </div>
+
+      <div className="flex flex-row ml-44 mr-44">
+        <div className="pt-32 w-[50%]">
+          <img src={hero} alt="" />
+          <img src={cloud} width={120} className="ml-[560px] -mt-20" alt="" />
+        </div>
+        <div className="w-[50%]">
+          <Title fontSize="5xl" className="pt-64 pl-28" weight="medium">
+            Naturally Better
+          </Title>
+          <Title
+            className="text-right pl-4 ml-1 w-[450px]"
+            weight="light"
+            marginTop="mt-10"
+            fontSize="md"
+            height="6"
+            letterSpace="wider"
+          >
+            Green Boom products are made from 100% sustainably sourced,
+            renewable raw materials. We’re also priced competitively so you
+            don’t have to choose between performance or the environment.
+          </Title>
+          <button
+            className="mt-10 ml-80 bg-primaryA text-white px-8 py-2 hover:bg-white hover:text-black rounded-md cursor-pointer"
+            onClick={handleButton}
+          >
+            Get Green
+          </button>
         </div>
       </div>
     </div>
